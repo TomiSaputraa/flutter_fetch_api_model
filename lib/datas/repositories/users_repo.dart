175ba6df955b627bcea4cbfb6b3dc0e5db2fcde3
Tomi Saputra <74924041+TomiSaputraa@users.noreply.github.com>
2022-08-25 15:40:55 +0700

    Update users_repo.dart

--- lib/datas/repositories/users_repo.dart
+++ lib/datas/repositories/users_repo.dart
@@ -5,26 +5,28 @@ import 'package:http/http.dart' as http;
 
 class Repository {
   Future<List<User>> getData() async {
-    var baseUrl = 'https://63070990c0d0f2b8012570e6.mockapi.io/api';
-    var response = await http.get(Uri.parse(baseUrl));
-    var jsonData = json.decode(response.body);
-    if (response.statusCode == 200) {
-      List<User> users = [];
+    List<User> users = [];
 
-      for (var u in jsonData) {
-        User user = User(
-            createdAt: DateTime.parse(u['createdAt']),
-            name: u['name'],
-            avatar: u['avatar'],
-            updateAt: DateTime.parse(u['createdAt']),
-            id: u['id']);
-
-        users.add(user);
-      }
-      print(jsonData);
-      return users;
-    } else {
-      throw Exception("Failed to load cat fact!");
+    try {
+      var baseUrl = 'https://63070990c0d0f2b8012570e6.mockapi.io/api';
+      var response = await http.get(Uri.parse(baseUrl));
+      var jsonData = json.decode(response.body);
+      if (response.statusCode == 200) {
+        for (var u in jsonData) {
+          User user = User(
+              createdAt: DateTime.parse(u['createdAt']),
+              name: u['name'],
+              avatar: u['avatar'],
+              updateAt: DateTime.parse(u['createdAt']),
+              id: u['id']);
+          users.add(user);
+        }
+        print(jsonData);
+        return users;
+      } //=> if you dont want to use try method just remove and its work
+    } catch (e) {
+      print(e.toString());
     }
+    return users;
   }
 }
